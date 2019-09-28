d05d87c10f19da66a09e568f2c9d8e9c92cf2b84
Glitch (a4-jamevor) <none>
2019-09-28 01:22:59 +0000

    😘🌽 Checkpoint ./public/visualizer.html:6360586/249 ./public/js/module1.js:6360586/481 ./public/js/module2.js:6360586/112

--- public/js/module1.js
+++ public/js/module1.js
@@ -1,3 +1,14 @@
-function myFunction() {
-console.log(5 + 6);
-  }
+import * as T from './js/three.js';
+
+export function myFunction(camera, renderer) {
+   new T.OrbitControls(camera, renderer.domElement);
+      controls.enableDamping = true;
+      controls.campingFactor = 0.25;
+      controls.enableZoom = true;
+  return 
+}
+
+export function myFunction2() {
+  console.log(5 + 6);
+}
+
---
+++ public/js/module2.js
@@ -0,0 +1,8 @@
+export function myFunction() {
+  console.log(5 + 6);
+}
+
+export function myFunction() {
+  console.log(5 + 6);
+}
+
