aaeeca6879ff2a08f52fd93ff36d6078211fa4e0
Glitch (a4-jamevor) <none>
2019-09-28 00:01:56 +0000

    🌪💷 Checkpoint ./public/js/scripts.js:6360586/2153

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,12 +1,12 @@
-$(document).ready(function() {
-    $('#fullTable').DataTable( {
-        "ajax": 'fullOrders.json'
-    } );
-} );
-
-
-$(document).ready(function() {
-    $('#userTable').DataTable( {
-        "ajax": 'userOrders.json'
-    } );
-} );
+   $(document).ready(function() {
+       $('#fullTable').DataTable( {
+           "ajax": 'fullOrders.json'
+       } );
+   } );
+   
+ 7  
+ 8  $(document).ready(function() {
+ 9      $('#userTable').DataTable( {
+10          "ajax": 'userOrders.json'
+11      } );
+12  } );
